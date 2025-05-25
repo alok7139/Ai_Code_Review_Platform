0d70ee1df31c38d0c6bb9368d9f779a3d055a99a
alok7139 <alokgarg7139@gmail.com>
2025-05-25 20:25:56 +0530

    commit

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,35 +1,66 @@
-import { useState } from 'react'
+import { useState , useEffect } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import 'prismjs/themes/prism-tomorrow.css'
+import prism from 'prismjs'
+import Editor from 'react-simple-code-editor'
+import Markdown from "react-markdown"
+const backendUrl = import.meta.env.VITE_BACKEND_URL;
+import axios from 'axios'
+
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const [prompt, setprompt] = useState( `function sum(){
+  return 1+1; }`)
+
+  const [review, setreview] = useState(``)
+
+  useEffect(() => {
+    prism.highlightAll();
+  } , [])
+
+  const reviewcode = async() => {
+      
+    try {
+      const res = await axios.post( `${backendUrl}/ai/get-response` , {prompt});
+      setreview(res.data);
+    } catch (error) {
+      console.log(error);
+    }
+  }
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <main>
+        <div className="left">
+          <div className="code">
+            <Editor 
+             value={prompt}
+             onValueChange={prompt => setprompt(prompt)}
+             highlight={prompt => prism.highlight(prompt ,prism.languages.javascript , "javascript")}
+             padding={10}
+             style={{
+              fontFamily : '"Fira code", "Fira Mono", monospace',
+                fontSize: 16,
+                border: "1px solid #ddd",
+                borderRadius: "5px",
+                height: "100%",
+                width: "100%"
+             }}
+            />
+          </div>
+          <div onClick={reviewcode} className="reviewbtn">Review</div>
+        </div>
+        <div className="right">
+         <Markdown> {review}</Markdown>
+        </div>
+      </main>
     </>
   )
 }
+
+
 
 export default App
